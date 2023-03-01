f06a2d97de29df4c409231f08f13303c74017dc8
ascanioneves <ascaniosavio97@gmail.com>
2023-02-28 23:22:21 -0300

    feat: adding mean strategy approach

--- 01_Preprocessing/test.py
+++ 01_Preprocessing/test.py
@@ -1,11 +1,11 @@
-from sklearn.impute import SimpleImputer
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
 import pandas as pd
-from sklearn.neighbors import KNeighborsClassifier
 import requests
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
-print('\n - Reading diabetes dataset ...')
+print('\nReading diabetes dataset ...')
 data = pd.read_csv('diabetes_dataset.csv')
 
 feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 
@@ -13,30 +13,53 @@ feature_cols = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness',
 
 X = data[feature_cols]
 y = data.Outcome
-#print(y)
+acc = 0
 
-#Splitting dataset into train and test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
+print("Generating 5 different results ...")
+for i in range(5):
 
-#Combining X_test and y_test with the purpose of dropping test set missing data
-combined = pd.concat([X_test, y_test], axis=1)
+    #Splitting dataset into train and test
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-#Dropping missing values of test set
-dropped_test = combined.dropna(axis=0, how='any')
-X_test = dropped_test[feature_cols]
-y_test = dropped_test.Outcome
+    #Combining X_test and y_test with the purpose of dropping test set missing data
+    combined = pd.concat([X_test, y_test], axis=1)
 
-#Combining X_train and y_train with the pyrpose of dropping train set missing data
-combined = pd.concat([X_train, y_train], axis=1)
-dropped_train = combined.dropna(axis=0, how='any')
-X_train = dropped_train[feature_cols]
-y_train = dropped_train.Outcome
-neigh = KNeighborsClassifier(n_neighbors=3)
-neigh.fit(X_train, y_train)
+    #Dropping missing values of test set
+    dropped_test = combined.dropna(axis=0, how='any')
+    X_test = dropped_test[feature_cols]
+    y_test = dropped_test.Outcome
 
-#Make predictions
-y_pred = neigh.predict(X_test)
+    #Combining X_train and y_train with the pyrpose of dropping train set missing data
+    #combined = pd.concat([X_train, y_train], axis=1)
+    #dropped_train = combined.dropna(axis=0, how='any')
+    #X_train = dropped_train[feature_cols]
+    #y_train = dropped_train.Outcome
+    imputer = SimpleImputer(strategy='mean')
+    df = pd.concat([X_train, y_train], axis=1)
+    imputer.fit(df)
+    imputed_data = imputer.transform(df)
+    df = pd.DataFrame(imputed_data, columns=df.columns)
+    X_train = df[feature_cols]
+    y_train = df.Outcome
+    neigh = KNeighborsClassifier(n_neighbors=3)
+    neigh.fit(X_train, y_train)
 
-#Accuracy
-print(f'Accuracy: {accuracy_score(y_test, y_pred)}')
+    #Make predictions
+    y_pred = neigh.predict(X_test)
 
+    #Accuracy
+    a = accuracy_score(y_test, y_pred)
+    print(f'Accuracy [{i}]: {a}')
+    acc += a
+
+log = './log.txt'
+with open(log, "r") as file:
+    content = file.read()
+
+new_content = content + '\n' + str(acc/5)
+obj = ' (using mean strategy)'
+
+with open(log, "w") as file:
+    file.write(new_content + obj)
+
+print(f'Mean of accuracy: {acc/5}')
